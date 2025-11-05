9966c3781a0a9074e40f02e2642bf0b6a58b2a11
vani <va.fuentes@alumno.um.edu.ar>
2025-11-05 20:54:35 -0300

    Prueba de conexion con el back de la nube

--- bitforce-frontend/src/utils/api.js
+++ bitforce-frontend/src/utils/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("API_BASE =>", API_BASE);
+
 /* ===== BASE ===== */
 const API_BASE = process.env.REACT_APP_API_BASE || "http://127.0.0.1:8000/api";
 const AUTH_STRATEGY = process.env.REACT_APP_AUTH_STRATEGY || "jwt";
